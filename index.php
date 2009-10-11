3bc8671afc0d725bf89b16462c4026f9bb344724
bruchmann.g <bruchmann.g@1178d8ca-aca0-11de-8ba5-91705c1537ed>
2009-10-11 15:19:09 +0000

    Cambie el nombre del test MapLoading a MapMoving, actualizé los resultados de Scroll-Left, empezé a codear el index y agregué archivos para el estilo general y los diferentes scripts

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>JSMario 2.0 -- The Super Mario complete reconstruction plus Map Editor.</title>
+	
+	<script type="text/javascript" src="works.js"></script>
+	<script type="text/javascript" src="precarga.js"></script>
+	<script type="text/javascript" src="eventos.js"></script>
+	<script type="text/javascript" src="nucleo.js"></script>
+	<script type="text/javascript" src="misc.js"></script>
+	<script type="text/javascript" src="resize.js"></script>
+	<script type="text/javascript" src="seres.js"></script>
+	<script type="text/javascript" src="mapa.js"></script>
+	
+	<link rel="stylesheet" type="text/css" href="styles/general.css" />
+</head>
+<body>
+	<div id="topWarper">
+		<div id="title">JSMario 2.0</div>
+		<div id="subTitle">Super Mario Bross totally reconstructed using only javascript.</div>
+	</div>
+	<div id="centerWarper">
+		
+	</div>
+	<div id="bottomWarper">
+		
+	</div>
+</body>
+</html>
